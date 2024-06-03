9cba08377c0748038c1d70a77e821abef605035e
zakaria <grarizakaria@gmail.com>
2024-06-03 19:57:32 +0100

    task3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to check if authentication is required. """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to get the authorization header from the request. """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get the current user. """
+        return None
