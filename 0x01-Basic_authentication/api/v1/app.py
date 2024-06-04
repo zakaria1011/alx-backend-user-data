79fb73ae2a880ce3e733e9a062fb1a52b8c731ad
zakaria <grarizakaria@gmail.com>
2024-06-04 19:35:28 +0100

    task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE", None)
 
-if AUTH_TYPE:
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,11 +4,14 @@ BasicAuth module for the API
 """
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """ BasicAuth class to handle basic authentication """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Extracts the Base64 part of the Authorization header for Basic Authentication """
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """ Extract Base64 part of the Authorization header """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
