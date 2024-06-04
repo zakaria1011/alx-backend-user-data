bf58a98f88e79e46b6a13c73dfb3285912780b8a
zakaria <grarizakaria@gmail.com>
2024-06-04 19:44:19 +0100

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth module for the API
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Decodes a Base64 string to a UTF-8 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
