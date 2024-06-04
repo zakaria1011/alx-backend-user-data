9ad21bad7a0d9488798a6b613afc6b2cab7d0e78
zakaria <grarizakaria@gmail.com>
2024-06-04 18:51:53 +0100

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,22 @@ from typing import List, TypeVar
 class Auth:
     """ auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if authentication is required. """
-        return False
+        """
+        Determines if authentication is required for a given path.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header from the request. """
