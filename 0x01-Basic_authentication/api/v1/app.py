f8ecddd1c890dd7506ea196de7d5787269671a4b
zakaria <grarizakaria@gmail.com>
2024-06-04 19:03:33 +0100

    task5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,10 +8,16 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE", None)
+
+if AUTH_TYPE:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -23,14 +29,39 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler """
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    Method to filter requests before reaching the route handlers.
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header from the request. """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method to get the current user. """
