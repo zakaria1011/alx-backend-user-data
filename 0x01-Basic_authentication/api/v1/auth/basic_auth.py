70e53d972504e34256f3affd6368fdef963a45a0
zakaria <grarizakaria@gmail.com>
2024-06-04 20:13:52 +0100

    task100

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,24 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+        
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        
+        b64_header = self.extract_base64_authorization_header(auth_header)
+        if b64_header is None:
+            return None
+        
+        decoded_header = self.decode_base64_authorization_header(b64_header)
+        if decoded_header is None:
+            return None
+        
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+        
+        return self.user_object_from_credentials(email, password)
+
