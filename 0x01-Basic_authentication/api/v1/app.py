231b2e34d69d70ff4344ddcfe29a0e6a56e6d1ca
zakaria <grarizakaria@gmail.com>
2024-06-05 19:40:20 +0100

    task0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -63,6 +63,7 @@ def before_request() -> None:
     if auth.authorization_header(request) is None:
         abort(401)
 
+    request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
 
