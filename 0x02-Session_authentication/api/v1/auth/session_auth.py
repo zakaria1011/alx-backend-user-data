d33c53007a6f8c3bc85f3697011e0d4e47cd44b7
zakaria <grarizakaria@gmail.com>
2024-06-06 12:28:32 +0100

    task8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,7 +39,7 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
-    
+
     def destroy_session(self, request=None):
         """Deletes de user session / logout"""
 
