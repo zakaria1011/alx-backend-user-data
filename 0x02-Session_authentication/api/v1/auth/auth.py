cc78180feca3734027511a4be68ac7389f0f7d88
zakaria <grarizakaria@gmail.com>
2024-06-06 11:39:14 +0100

    task4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ from flask import request
 from typing import List, TypeVar
 import os
 
+
 class Auth:
     """ auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
