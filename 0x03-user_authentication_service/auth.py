a3ac72bc6d8d242db230e54406b4c23f5c57d94e
zakaria <grarizakaria@gmail.com>
2024-06-11 18:34:40 +0100

    task4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" authentification module"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a salted hash of the input password """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
